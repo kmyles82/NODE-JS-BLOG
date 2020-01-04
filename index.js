e77291b9799e274acce83ff2c8f5bf20ab57fe06
kmyles82 <kmyles82@gmail.com>
2020-01-04 10:58:50 -0600

    installed express-edge, setup template engine, created views folder

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const express = require('express');
 const path = require('path');
+const {
+    config,
+    engine
+} = require('express-edge');
+const express = require('express');
 
-const app = new express();
+const app = new express()
 
 app.use(express.static('public'));
 
+app.use(engine);
+app.set('views', `${__dirname}/views`);
+
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+    res.render('index');
+})
+
+app.get('/', (req, res) => {
+    edge.render('index')
 })
 
 app.get('/about', (req, res) => {
