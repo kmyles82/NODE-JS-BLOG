92ca61d25d778e929ab0b1aa01cf9e7cb8333261
kmyles82 <kmyles82@gmail.com>
2020-01-03 20:02:28 -0600

    copied bootstrap template to public folder

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const path = require('path');
+
 const app = new express();
 
+app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+})
 
 app.listen(4000, (req, res) => {
     console.log('App listening on port 4000');
