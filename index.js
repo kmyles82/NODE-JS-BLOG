4ecdbd926e24921b43c67afa57487a609c31c9fc
kmyles82 <kmyles82@gmail.com>
2020-01-04 12:39:39 -0600

    added create.edge and its route

--- index.js
+++ index.js
@@ -34,6 +34,10 @@ app.get('/contact', (req, res) => {
     res.render('contact')
 })
 
+app.get('/post/new', (req, res) => {
+    res.render('create')
+})
+
 app.listen(4000, (req, res) => {
     console.log('App listening on port 4000');
 })
--- test.js
+++ test.js
@@ -6,10 +6,24 @@ mongoose.connect('mongodb://localhost/node-js-test-blog', {
     useUnifiedTopology: true
 })
 
-Post.create({
-    title: 'My first blog post',
-    description: 'Blog post description',
-    contents: 'Lorem ipsum content'
-}, (error, post) => {
-        console.log(error, post)
+// Post.create({
+//     title: 'My Second blog post',
+//     description: 'Second post description',
+//     contents: 'Second Lorem ipsum content'
+// }, (error, post) => {
+//         console.log(error, post)
+// })
+
+Post.find({}, (error, post) => {
+    console.log(error, post)
 })
+
+// Post.findById("5e10d621b680db6448b1395c", (error, post) => {
+//     console.log(error, post)
+// })
+
+// Post.findByIdAndUpdate('5e10d621b680db6448b1395c', {
+//     title: 'My first blog post title lorem ipsum'
+// }, (error, post) => {
+//     console.log(error, post)
+// })
