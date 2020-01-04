24d4297463f21565f44ed17cfb840158fc7ba84e
kmyles82 <kmyles82@gmail.com>
2020-01-04 11:46:50 -0600

    installed mongoose , created db connection and create PostSchema

---
+++ database/models/Post.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const PostSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    contents: String
+});
+
+const Post = mongoose.model('Post', PostSchema);
+
+module.exports = Post;
