379417f99a80f4865e1c35e59f6950d4ac674af7
kmyles82 <kmyles82@gmail.com>
2020-01-06 19:55:14 -0600

    setup login progress

--- controllers/loginUser.js
+++ controllers/loginUser.js
@@ -1,8 +1,27 @@
 const User = require('../database/models/User')
+const bcrypt = require('bcrypt')
 
 module.exports = (req, res) => {
     
+    const { email, password } = req.body;
     
-    res.redirect('/')
+    //try to find user
+    User.findOne({ email: email }, (error, user) => {
+        if (user) {
+            //compare user password
+            bcrypt.compare(password, user.password, (error, samePassword) => {
+                //if user password is correct login user
+                if (samePassword) {
+                    //store user session
 
+                    res.redirect('/');
+                } else {
+                    //else redirect user back
+                    res.redirect('/auth/login');
+                }
+            })
+        } else {
+            return res.redirect('/auth/login');
+        }
+    }) 
 }
